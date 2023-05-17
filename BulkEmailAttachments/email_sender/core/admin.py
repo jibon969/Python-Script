a91601b0de8ed458ea6f56b79aa31af00c3fb20a
jayed969 <jayed.swe@gmail.com>
2023-05-17 22:00:40 +0600

    remove bulk email

--- BulkEmailAttachments/email_sender/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Emailer, EmailContacts
-# Register your models here.
-admin.site.register(Emailer)
-admin.site.register(EmailContacts)
--- BulkEmailAttachments/email_sender/core/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import EmailCreateView
-from django.urls import path
-app_name ='emailer'
-
-urlpatterns = [
-  path('', EmailCreateView.as_view(),name='emailer')
-]
